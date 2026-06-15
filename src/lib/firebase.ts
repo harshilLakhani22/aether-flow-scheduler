c547a9c8a35800d56e1f78a71547d6821ed58837
harshilLakhani22 <harshillakhani2264@gmail.com>
2026-06-15 19:29:20 +0530

    chore: hardcode firebase config for Vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyA5UgOLHDmhtwxY7_u35DRamxD8-dg16Jg",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "taskpad-4944a.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "taskpad-4944a",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "taskpad-4944a.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "744929744591",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:744929744591:web:88785457bbb4be5a8609ba",
+  measurementId: "G-H7YRKBHJDM",
 };
 
 // Initialize Firebase only if config is provided to avoid crashing before setup
